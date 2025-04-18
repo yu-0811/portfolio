07ba2325aac41578d171ef7d12f78c642f84c0bf
yu-811 <teradayu0811@gmail.com>
2025-04-19 00:16:20 +0900

    Update

--- src/lib/supabaseServer.ts
+++
@@ -1,6 +0,0 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-
-export function createClient() {
-  return createServerComponentClient<any>({ cookies });
-}
