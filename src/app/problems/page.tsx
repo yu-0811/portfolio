c0554499ae4104161933bca194d29864f948bea3
yu-811 <teradayu0811@gmail.com>
2025-04-19 09:21:55 +0900

    fix

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -1,5 +1,4 @@
 import { supabase } from "@/lib/supabaseClient";
-import Link from "next/link";
 
 export default async function ProblemListPage() {
   const { data: problems, error } = await supabase
